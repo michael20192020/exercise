24d35b75e09682f40e80bea0517544642a40c6b5
michael20192020 <zhuqi2012@hotmail.com>
2021-12-10 20:30:30 +1100

    create ListViewHoler class

---
+++ app/src/main/java/com/hansoft/exercise/ListViewHolder.kt
@@ -0,0 +1,28 @@
+package com.hansoft.exercise
+
+/**
+ * created by qi zhu 10/12/2011
+ * Create ListViewHolder class to show the title,description and imageHref information on the list_item.xml
+ */
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+
+    var mTitle: TextView? = null
+    var mDescription: TextView? = null
+    var imageView: ImageView? = null
+
+    init {
+       
+        mTitle = itemView!!.findViewById(R.id.item_title)
+        mDescription = itemView!!.findViewById<View>(R.id.item_description) as TextView
+        imageView = itemView!!.findViewById(R.id.item_imageHref)
+
+    }
+
+
+}
