534f5fa1fc93a7086ff9704c0e1d08e199cb4975
michael20192020 <zhuqi2012@hotmail.com>
2021-12-10 16:57:26 +1100

    create Canada class comment

--- app/src/main/java/com/hansoft/exercise/Canada.kt
+++ app/src/main/java/com/hansoft/exercise/Canada.kt
@@ -1,5 +1,11 @@
 package com.hansoft.exercise
 
+
+/**
+  * created by qi zhu 10/12/2011
+  * Create Canada class to store the title,description and imageHref information
+ */
+
 class Canada {
     var title: String? = null
     var description: String? = null
